db2902228b73cdedc80836227b8dd76ea9301841
hoseong091234 <hoseong091234@gmail.com>
2020-04-16 12:32:10 +0900

    digitalClock

--- Chapter6_practice/example6-1_textclock/src/main/java/com/example/example6_1_textclock/MainActivity.java
+++ Chapter6_practice/example6-1_textclock/src/main/java/com/example/example6_1_textclock/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.example6_1_textclock;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextClock;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextClock textClock;
+    TextView textView;
+    Button btnGetTime;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textClock = findViewById(R.id.textClock);
+        textView=  findViewById(R.id.textView);
+        btnGetTime = findViewById(R.id.button);
+
+        btnGetTime.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                textView.setText("Time: "+textClock.getText());
+            }
+        });
     }
 }
