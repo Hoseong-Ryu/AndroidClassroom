2cb74c3ac94086a8654a018b115cab4b5e5a73d8
hoseong091234 <hoseong091234@gmail.com>
2020-05-25 13:50:47 +0900

    activity

--- Chapter10/ex01_practice10-1/src/main/java/com/example/ex01_practice10_1/MainActivity.java
+++ Chapter10/ex01_practice10-1/src/main/java/com/example/ex01_practice10_1/MainActivity.java
@@ -11,7 +11,7 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    RadioButton ac2, ac3;
+    RadioButton rd1, rd2;
     Button btn;
 
     @Override
@@ -19,22 +19,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ac2 = findViewById(R.id.act2);
-        ac3 = findViewById(R.id.act3);
+        rd1 = findViewById(R.id.act2);
+        rd2 = findViewById(R.id.act3);
         btn = findViewById(R.id.move_act);
-        ac2.setOnClickListener(new View.OnClickListener() {
+        rd1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(ac3.isChecked()){
-                    ac3.setChecked(false);
+                if(rd2.isChecked()){
+                    rd2.setChecked(false);
                 }
             }
         });
-        ac3.setOnClickListener(new View.OnClickListener() {
+        rd2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(ac2.isChecked()){
-                    ac2.setChecked(false);
+                if(rd1.isChecked()){
+                    rd1.setChecked(false);
                 }
             }
         });
@@ -42,10 +42,10 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent;
-                if(ac2.isChecked()){
+                if(rd1.isChecked()){
                     intent = new Intent(getApplicationContext(), SecondActivity.class);
                     startActivity(intent);
-                } else if (ac3.isChecked()){
+                } else if (rd2.isChecked()){
                     intent = new Intent(getApplicationContext(), ThirdActivity.class);
                     startActivity(intent);
                 } else {
